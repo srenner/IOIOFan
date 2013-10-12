330055799c65c615950622d6772b91aa930f2d51
Shawn Renner <shawn.renner@gmail.com>
2013-10-12 10:45:44 -0500

    Set PWM via service

--- src/com/srenner/ioiofan/FanService.java
+++ src/com/srenner/ioiofan/FanService.java
@@ -1,61 +1,84 @@
 package com.srenner.ioiofan;
 
+import ioio.lib.api.DigitalOutput;
+import ioio.lib.api.IOIO;
+import ioio.lib.api.PulseInput;
+import ioio.lib.api.PwmOutput;
+import ioio.lib.api.DigitalInput.Spec;
+import ioio.lib.api.PulseInput.ClockRate;
+import ioio.lib.api.PulseInput.PulseMode;
+import ioio.lib.api.exception.ConnectionLostException;
+import ioio.lib.util.BaseIOIOLooper;
+import ioio.lib.util.IOIOLooper;
+import ioio.lib.util.android.IOIOService;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
 
-public class FanService extends Service {
+public class FanService extends IOIOService {
 
-    int mStartMode;       // indicates how to behave if the service is killed
-    IBinder mBinder;      // interface for clients that bind
-    boolean mAllowRebind; // indicates whether onRebind should be used
+	
+	@Override
+protected IOIOLooper createIOIOLooper() {
+	return new BaseIOIOLooper() {
+		private DigitalOutput mLED;
+		private PwmOutput mPWM;
+		private PulseInput mTachSignal;
 
-    @Override
-    public void onCreate() {
-        // The service is being created
-    }
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        // The service is starting, due to a call to startService()
-    	
-		Notification notification = new NotificationCompat.Builder(this)
-		.setSmallIcon(R.drawable.ic_launcher)
-		.setContentTitle("Test title")
-		.setContentText("Test text")
-		.build();
-		Intent notificationIntent = new Intent(this, FanService.class);
-		PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
-		notification.contentIntent = pendingIntent;
-		NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-		notificationManager.notify(1, notification);
+		@Override
+		protected void setup() throws ConnectionLostException, InterruptedException {
+			mPWM = ioio_.openPwmOutput(1, 25000);
+			Spec s = new Spec(2);
+			mTachSignal = ioio_.openPulseInput(s, ClockRate.RATE_62KHz, PulseMode.FREQ, true);
+			mLED = ioio_.openDigitalOutput(IOIO.LED_PIN);
+		}
+		
+		@Override
+		public void loop() throws ConnectionLostException, InterruptedException {
+			
+			
+			mPWM.setPulseWidth(20);
+			final int rpm = Math.round(mTachSignal.getFrequency() * 30);
+			
+			//flash LED to test that service is running
+			mLED.write(false);
+			Thread.sleep(500);
+			mLED.write(true);
+			Thread.sleep(500);
+		}
+	};
+}
 
-		startForeground(1, notification);
-    	
-        return mStartMode;
-    }
-    @Override
-    public IBinder onBind(Intent intent) {
-        // A client is binding to the service with bindService()
-        return mBinder;
-    }
-    @Override
-    public boolean onUnbind(Intent intent) {
-        // All clients have unbound with unbindService()
-        return mAllowRebind;
-    }
-    @Override
-    public void onRebind(Intent intent) {
-        // A client is binding to the service with bindService(),
-        // after onUnbind() has already been called
-    }
-    @Override
-    public void onDestroy() {
-        // The service is no longer used and is being destroyed
-    }
+	@Override
+	public void onStart(Intent intent, int startId) {
+		super.onStart(intent, startId);
+		NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+		if (intent != null && intent.getAction() != null
+				&& intent.getAction().equals("stop")) {
+			// User clicked the notification. Need to stop the service.
+			nm.cancel(0);
+			stopSelf();
+		} 
+		else {
+			// Service starting. Create a notification.
+			
+			Notification notification = new NotificationCompat.Builder(this)
+			.setSmallIcon(R.drawable.ic_launcher)
+			.setContentTitle("IOIOFan Service")
+			.setContentText("Click to stop")
+			.setContentIntent(PendingIntent.getService(this, 0, new Intent(
+					"stop", null, this, this.getClass()), 0))
+			.build();
+			notification.flags |= Notification.FLAG_ONGOING_EVENT;
+			nm.notify(0, notification);
+		}
+	}	
 
+	@Override
+	public IBinder onBind(Intent arg0) {
+		return null;
+	} 
 }
--- src/com/srenner/ioiofan/MainActivity.java
+++ src/com/srenner/ioiofan/MainActivity.java
@@ -1,15 +1,7 @@
 package com.srenner.ioiofan;
 
-import ioio.lib.api.DigitalInput.Spec;
-import ioio.lib.api.DigitalOutput;
-import ioio.lib.api.PulseInput;
-import ioio.lib.api.PulseInput.ClockRate;
-import ioio.lib.api.PulseInput.PulseMode;
-import ioio.lib.api.PwmOutput;
-import ioio.lib.api.exception.ConnectionLostException;
-import ioio.lib.util.BaseIOIOLooper;
-import ioio.lib.util.IOIOLooper;
-import ioio.lib.util.android.IOIOActivity;
+
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -17,7 +9,7 @@ import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
-public class MainActivity extends IOIOActivity {
+public class MainActivity extends Activity {
 
 	protected SeekBar mSeekPWM;
 	protected TextView mTvPWM;
@@ -68,73 +60,6 @@ public class MainActivity extends IOIOActivity {
 		tv.setText(text);
 	}
 	
-	/**
-	 * This is the thread on which all the IOIO activity happens. It will be run
-	 * every time the application is resumed and aborted when it is paused. The
-	 * method setup() will be called right after a connection with the IOIO has
-	 * been established (which might happen several times!). Then, loop() will
-	 * be called repetitively until the IOIO gets disconnected.
-	 */
-	class Looper extends BaseIOIOLooper {
-		private PwmOutput mPWM;
-		private PulseInput mTachSignal;
-		private DigitalOutput mLED;
 
-		/**
-		 * Called every time a connection with IOIO has been established.
-		 * Typically used to open pins.
-		 * 
-		 * @throws ConnectionLostException
-		 *             When IOIO connection is lost.
-		 * 
-		 * @see ioio.lib.util.AbstractIOIOActivity.IOIOThread#setup()
-		 */
-		@Override
-		protected void setup() throws ConnectionLostException {
-			mPWM = ioio_.openPwmOutput(PIN_PWM, 25000);
-			Spec s = new Spec(PIN_RPM);
-			mTachSignal = ioio_.openPulseInput(s, ClockRate.RATE_62KHz, PulseMode.FREQ, true);
-			mLED = ioio_.openDigitalOutput(0, false);
-		}
 
-		/**
-		 * Called repetitively while the IOIO is connected.
-		 * 
-		 * @throws ConnectionLostException
-		 *             When IOIO connection is lost.
-		 * 
-		 * @see ioio.lib.util.AbstractIOIOActivity.IOIOThread#loop()
-		 */
-		@Override
-		public void loop()  {
-			try {
-				
-				int val = mSeekPWM.getProgress();
-				mPWM.setPulseWidth(val);
-				//signal goes high twice per rotation. multiply by 30 to get RPM
-				final int rpm = Math.round(mTachSignal.getFrequency() * 30);
-				
-				runOnUiThread(new Runnable(){
-				    public void run(){
-				    	mTvRPM.setText(String.valueOf(rpm));
-				    }
-				});
-				
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-			} catch (ConnectionLostException e) {
-			} catch (Exception e) {
-			}
-		}
-	}
-
-	/**
-	 * A method to create our IOIO thread.
-	 * 
-	 * @see ioio.lib.util.AbstractIOIOActivity#createIOIOThread()
-	 */
-	@Override
-	protected IOIOLooper createIOIOLooper() {
-		return new Looper();
-	}
 }
